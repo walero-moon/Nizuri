87c8d58770c333ab584eb9b2ae4a08f7a862b60b
AnUnawarewolf <royal.victor.leopoldino@gmail.com>
2021-06-17 17:48:26 -0300

    Help command

--- commands/Dev only/args-info.js
+++ commands/Dev only/args-info.js
@@ -1,8 +1,9 @@
 module.exports = {
 	name: 'args-info',
 	description: 'Information about the arguments provided.',
-    args: true,
-    usage: '<arg1> <arg2>...<arg n>',
+  args: true,
+  aliases: ['args'],
+  usage: '<arg1> <arg2>...<arg n>',
 	execute(message, args) {
 		if (args[0] === 'foo') {
 			return message.channel.send('bar');
--- index.js
+++ index.js
@@ -5,43 +5,52 @@ const Discord = require('discord.js')
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+// Loop through commands folder and load different commands
 const commandFolders = fs.readdirSync('./commands')
 for (const folder of commandFolders) {
-    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'))
+  const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'))
 
-    for (const file of commandFiles) {
-        const command = require(`./commands/${folder}/${file}`);
-        client.commands.set(command.name, command);
-    }
+  for (const file of commandFiles) {
+      const command = require(`./commands/${folder}/${file}`);
+      client.commands.set(command.name, command);
+  }
 }
 
+// On ready do this
 client.once('ready', () => {
 	console.log('Nizuri is ready!');
 });
 
+
 client.on('message', (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+  // Check if message starts with the prefix or if it isn't a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
+  // Slice prefix, and get arguments
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const commandName = args.shift().toLowerCase(); // Set command name to be arg[0] and remove it from args
 
-    if (!client['commands'].has(commandName)) return;
+  const command = client.commands.get(commandName) // Get command or checks for aliases
+    || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    
+  if (!command) return; // return if command not found
 
-    const command = client['commands'].get(commandName);
+  // Check if command needs args and if the user passed arguments
+  if (command.args && !args.length) {
+      let reply = `You didn't provide any arguments, ${message.author}!`
 
-    if (command.args && !args.length) {
-        let reply = `You didn't provide any arguments, ${message.author}!`
-        if (command.usage) {
-            reply += `\nUsage: \`${prefix}${command.name} ${command.usage}\``
-        }
-        return message.channel.send(reply)
-    }
+      // Checks if proper usage exists, if it does, add the proper usage to the reply
+      if (command.usage) {
+          reply += `\nUsage: \`${prefix}${command.name} ${command.usage}\``
+      }
+      return message.channel.send(reply)
+  }
 
-    try {
-        command.execute(message, args);
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+      command.execute(message, args); // Executes command, duh
+  } catch (error) {
+      console.log(error);
+  }
 })
 
 client.login(token);
