d02d2afe23ed73a96efcd47ccea0b1b029068e68
Victor <royal.victor.leopoldino@gmail.com>
2021-08-01 16:38:10 -0700

    Change reusing to using on command reusage

--- index.ts
+++ index.ts
@@ -93,7 +93,7 @@ client.on('message', (message) => {
 
         if (now < expirationTime) {
             const timeLeft = (expirationTime - now) / 1000;
-            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before using the \`${command.name}\` command.`);
         }
     }
     timestamps.set(message.author.id, now);
